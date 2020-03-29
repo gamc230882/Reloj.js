4b6d7b74f6ee82d46b0abb14b3fa77e4bc2b2919
Gustavo Morillo <gamc230882@gmail.com>
2020-03-29 19:59:21 -0300

    Segundo commit Falta cuadrar las horas

--- main.js
+++ main.js
@@ -1,6 +1,51 @@
 (function(){
 
+  var actualizarHora = function(){
+      var fecha = new Date(),
+          horas = fecha.getHours(),
+          minutos = fecha.getMinutes(),
+          segundos = fecha.getSeconds(),
+          // ampm,
+          diaSemana = fecha.getDay(),
+          dia = fecha.getDate(),
+          mes = fecha.getMonth(),
+          year = fecha.getFullYear();
+
+      var pHoras = document.getElementById("horas"),
+          pMinutos = document.getElementById("minutos"),
+          pSegundos = document.getElementById("segundos"),
+          pAMPM = document.getElementById("ampm"),
+          pDiaSemana = document.getElementById("diaSemana"),
+          pDia = document.getElementById("dia"),
+          pMes = document.getElementById("mes"),
+          pYear = document.getElementById("year");
 
 
+      var semana = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
+          pDiaSemana.textContent = semana[diaSemana];
+
+          var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+              pMes.textContent = meses[mes];
+
+          pDia.textContent = dia;
+
+          pYear.textContent = year;
+
+          pHoras.textContent = horas;
+
+          pMinutos.textContent = minutos;
+
+          pSegundos.textContent = segundos;
+
+          // pAMPM.textContent = ampm;
+
+
+
+
+
+
+  };
+
+  actualizarHora()
 
 }());
